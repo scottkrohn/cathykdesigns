97c13e0bd46243d3caf6e11dd875c1d70f666a6b
Scott Krohn <skrohn@revolutionparts.com>
2018-09-22 00:00:12 -0700

    Set port number

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 import express from 'express';
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
-import webpackConfig from './webpack.config.js';
 import path from 'path';
 
-const app = express();
+// TODO: Should prd/dev have different config files?
+import webpackConfig from './webpack.config.js';
 
+const app = express();
 const env = app.get('env');
+
 if (env === 'production') {
+	// Serve static output from webpack for production.
 	app.use(express.static(path.join(__dirname, 'build')))
 } else {
+	// Serve react code with webpack for development.
 	app.use(webpackMiddleware(webpack(webpackConfig)));
 }
 
+const port = (process.env.PORT || 3000);
 
-
-app.listen(3000, () => {
-	console.log('Listening on port 3000');
+app.listen(port, () => {
+	console.log(`Server running and listening on port ${port}`);
 });

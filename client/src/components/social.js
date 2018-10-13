b09670da6febd3a35ec8aa165410e6d60e3b3ccb
Scott Krohn <skrohn@revolutionparts.com>
2018-10-12 23:03:14 -0700

    Added social icons

--- client/src/components/social.js
+++ client/src/components/social.js
@@ -1,10 +1,15 @@
 import React, { PureComponent } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faEtsy, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 
 class Social extends PureComponent {
 	render() {
 		return (<div>
-			<FontAwesomeIcon icon="facebook" />
+			<FontAwesomeIcon icon={faFacebookF} /><br />
+			<FontAwesomeIcon icon={faEtsy} /><br />
+			<FontAwesomeIcon icon={faTwitter} /><br />
+			<FontAwesomeIcon icon={faInstagram} />
 		</div>);
 	}
 }

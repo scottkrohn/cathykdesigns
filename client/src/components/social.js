233a8cc54e1732a62e2b6d4dfc51f8c6419415cc
Scott Krohn <skrohn@revolutionparts.com>
2018-10-12 22:58:09 -0700

    Added font awesome brands icons

--- client/src/components/social.js
+++ client/src/components/social.js
@@ -1,10 +1,12 @@
 import React, { PureComponent } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF} from '@fortawesome/free-brands-svg-icons';
+
 
 class Social extends PureComponent {
 	render() {
 		return (<div>
-			<FontAwesomeIcon icon="facebook" />
+			<FontAwesomeIcon icon={faFacebookF} />
 		</div>);
 	}
 }

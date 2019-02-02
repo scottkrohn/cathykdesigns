c280387a5d8a3e4401f70f98e7dbcf164295473c
Scott Krohn <skrohn@revolutionparts.com>
2019-02-02 05:22:23 -0700

    Material test

--- client/src/containers/faq-container.js
+++ client/src/containers/faq-container.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Dialog from '@material-ui/core/Dialog';
 
 class FaqContainer extends Component {
 	render() {
 		return (
 			<div>
-				I am a FAQ.
+				<Dialog open={true}>Hello</Dialog>
 			</div>
 		);
 	}

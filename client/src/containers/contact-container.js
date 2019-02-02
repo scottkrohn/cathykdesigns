2604895823f620bae2532a450dc5445827937442
Scott Krohn <skrohn@revolutionparts.com>
2019-02-01 21:20:17 -0700

    Moved everything from client specific package.json to project specific package.json

--- client/src/containers/contact-container.js
+++ client/src/containers/contact-container.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 
 class ContactContainer extends Component {
 	render() {
 		return (
 			<div>
 				I'm the contact us page!
+				<Button type="primary">hello</Button>
 			</div>
 		);
 	}
--- client/src/index.js
+++ client/src/index.js
@@ -9,9 +9,9 @@ import rootReducer from './reducers/root-reducer';
 
 import thunk from "redux-thunk";
 
-import "../node_modules/jquery/dist/jquery.min.js";
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/jquery/dist/jquery.min.js';
+import '../../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import App from './app';
 

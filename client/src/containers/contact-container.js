ffe80f90f5a085c3399257de2830bef05fd828b7
Scott Krohn <skrohn@revolutionparts.com>
2019-02-01 22:34:49 -0700

    Added proposed class properties

--- client/src/containers/contact-container.js
+++ client/src/containers/contact-container.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 
 class ContactContainer extends Component {
+
 	render() {
 		return (
 			<div>
 				I'm the contact us page!
-				<Button variant="contained" color="primary">hello</Button>
-				<Button variant="contained" color="primary">hello</Button>
-				<Button variant="contained" color="primary">hello</Button>
 			</div>
 		);
 	}
